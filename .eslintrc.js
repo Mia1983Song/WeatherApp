961beb863cdd0f7d1f1502d654dd7a5c1abca8bb
Mia Song <mia.song@soohoobook.com>
2025-04-25 10:54:00 +0800

    refactor

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
-};
+  extends: ['@react-native', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+}
