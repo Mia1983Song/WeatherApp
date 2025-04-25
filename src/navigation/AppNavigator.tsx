b85a95163d4d8b7139c9d23b1a02c204b4b37265
Mia Song <mia.song@soohoobook.com>
2025-04-25 10:35:41 +0800

    refactor

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,58 +1,60 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/Ionicons';
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/Ionicons'
 
-import HomeScreen from '../screens/HomeScreen';
-import SearchScreen from '../screens/SearchScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+import HomeScreen from '../screens/HomeScreen'
+import SearchScreen from '../screens/SearchScreen'
+import SettingsScreen from '../screens/SettingsScreen'
 
 // 集中定義導航路由及其所需參數
 export type RootTabParamList = {
-    Home: undefined;
-    Search: undefined;
-    Settings: undefined; // undefined 表示這些路由在被導航到時，不需要 任何參數
-};
+  Home: undefined
+  Search: undefined
+  Settings: undefined // undefined 表示這些路由在被導航到時，不需要 任何參數
+}
 
-const Tab = createBottomTabNavigator<RootTabParamList>();
+const Tab = createBottomTabNavigator<RootTabParamList>()
 
 export default function AppNavigator() {
-    return (
-        <Tab.Navigator
-            screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                    let iconName;
-                    // 根據路由名稱設定圖示
-                    if (route.name === 'Home') {
-                        iconName = focused ? 'home' : 'home-outline';
-                    } else if (route.name === 'Search') {
-                        iconName = focused ? 'search' : 'search-outline';
-                    } else if (route.name === 'Settings') {
-                        iconName = focused ? 'settings' : 'settings-outline';
-                    }
-                    return <Icon name={iconName || 'help-circle'} size={size} color={color} />;
-                },
-                tabBarActiveTintColor: '#5091e6', // 選中時的顏色
-                tabBarInactiveTintColor: 'gray', // 未選中時的顏色
-                // 如果 Tab 內還有 Stack，可能需要隱藏 Tab 的 header
-                //headerShown: false,
-            })}
-        >
-            {/* 各個分頁 */}
-            <Tab.Screen
-                name="Home"
-                component={HomeScreen}
-                options={{ title: '首頁' }}
-            />
-            <Tab.Screen
-                name="Search"
-                component={SearchScreen}
-                options={{ title: '搜尋' }}
-            />
-            <Tab.Screen
-                name="Settings"
-                component={SettingsScreen}
-                options={{ title: '設定' }}
-            />
-        </Tab.Navigator>
-    );
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName
+          // 根據路由名稱設定圖示
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline'
+          } else if (route.name === 'Search') {
+            iconName = focused ? 'search' : 'search-outline'
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings' : 'settings-outline'
+          }
+          return (
+            <Icon name={iconName || 'help-circle'} size={size} color={color} />
+          )
+        },
+        tabBarActiveTintColor: '#5091e6', // 選中時的顏色
+        tabBarInactiveTintColor: 'gray', // 未選中時的顏色
+        // 如果 Tab 內還有 Stack，可能需要隱藏 Tab 的 header
+        //headerShown: false,
+      })}
+    >
+      {/* 各個分頁 */}
+      <Tab.Screen
+        name='Home'
+        component={HomeScreen}
+        options={{ title: '首頁' }}
+      />
+      <Tab.Screen
+        name='Search'
+        component={SearchScreen}
+        options={{ title: '搜尋' }}
+      />
+      <Tab.Screen
+        name='Settings'
+        component={SettingsScreen}
+        options={{ title: '設定' }}
+      />
+    </Tab.Navigator>
+  )
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,81 +1,81 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import { getWeatherByCity, WeatherData } from '../api/weatherApi';
-import WeatherCard from '../components/WeatherCard';
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { getWeatherByCity, WeatherData } from '../api/weatherApi'
+import WeatherCard from '../components/WeatherCard'
 
 export default function HomeScreen() {
-    // [currentValue, setterFunction] = useState(initValue)
-    const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
+  // [currentValue, setterFunction] = useState(initValue)
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
 
-    // useEffect 主要用途：
-    // 1.Component 初始化 時做某些事（比如從 API 抓資料）
-    // 2.特定狀態（State）變更時，觸發副作用（Side Effect）
-    // 3.Component 卸載（Unmount）時，清理（Cleanup）資源（例如解除監聽、清除 timer）
-    // 第二個參數是依賴陣列，控制重新執行的時機
+  // useEffect 主要用途：
+  // 1.Component 初始化 時做某些事（比如從 API 抓資料）
+  // 2.特定狀態（State）變更時，觸發副作用（Side Effect）
+  // 3.Component 卸載（Unmount）時，清理（Cleanup）資源（例如解除監聽、清除 timer）
+  // 第二個參數是依賴陣列，控制重新執行的時機
 
-    // 元件掛載 (componentDidMount)
-    useEffect(() => {
-        console.log('元件掛載完成');
+  // 元件掛載 (componentDidMount)
+  useEffect(() => {
+    console.log('元件掛載完成')
 
-        fetchData();
+    fetchData()
 
-        // 元件卸載 (componentWillUnmount)
-        return () => {
-            console.log('元件即將卸載');
-            // 清理訂閱、計時器等資源
-        };
-    }, []); // 空依賴陣列表示只執行一次
+    // 元件卸載 (componentWillUnmount)
+    return () => {
+      console.log('元件即將卸載')
+      // 清理訂閱、計時器等資源
+    }
+  }, []) // 空依賴陣列表示只執行一次
 
-    // 資料更新 (componentDidUpdate)
-    useEffect(() => {
-        if (weatherData) {
-            console.log('資料已更新:', weatherData);
-        }
-    }, [weatherData]); // 依賴於 weatherData，當 weatherData 變化時執行
+  // 資料更新 (componentDidUpdate)
+  useEffect(() => {
+    if (weatherData) {
+      console.log('資料已更新:', weatherData)
+    }
+  }, [weatherData]) // 依賴於 weatherData，當 weatherData 變化時執行
 
-    const fetchData = async () => {
-        const cityName = 'Taipei';
+  const fetchData = async () => {
+    const cityName = 'Taipei'
 
-        try {
-            const data = await getWeatherByCity(cityName);
-            if (data) {
-                setWeatherData(data);
-            } else {
-                console.log("No Data");
-            }
-        } catch (e) {
-            setError('無法讀取天氣資料');
-        } finally {
-            setLoading(false);
-        }
-    };
+    try {
+      const data = await getWeatherByCity(cityName)
+      if (data) {
+        setWeatherData(data)
+      } else {
+        console.log('No Data')
+      }
+    } catch (e) {
+      setError('無法讀取天氣資料')
+    } finally {
+      setLoading(false)
+    }
+  }
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>首頁</Text>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>首頁</Text>
 
-            {/* Conditional Rendering */}
-            {error && <Text style={styles.error}>{error}</Text>}
-            {weatherData ? <WeatherCard weatherData={weatherData} /> : null}
-        </View>
-    );
+      {/* Conditional Rendering */}
+      {error && <Text style={styles.error}>{error}</Text>}
+      {weatherData ? <WeatherCard weatherData={weatherData} /> : null}
+    </View>
+  )
 }
 
 // 樣式
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#f5f5f5'
-    },
-    text: {
-        fontSize: 24,
-        fontWeight: 'bold'
-    },
-    error: {
-        color: 'red'
-    }
-});
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  error: {
+    color: 'red',
+  },
+})
